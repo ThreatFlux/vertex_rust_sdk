b0e2c3e452e3394a18f965c0c3b13e69031b92aa
Wyatt Roersma <wyatt@threatflux.com>
2026-03-23 13:04:37 -0400

    chore: upgrade dependencies and release flow

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,6 +1,7 @@
 //! Benchmarks for Vertex Rust SDK
 
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use std::hint::black_box;
 use threatflux_vertex_rust_sdk::{
     ChatMessage, Content, CountTokensRequest, GenerateContentRequest, GenerationConfig, Part,
 };
